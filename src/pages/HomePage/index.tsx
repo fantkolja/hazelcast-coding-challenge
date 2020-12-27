f06bd29e1b3550a381e466cfe9fe4fe97f0877b5
Mykola Fant <Mykola.Fant@infopulse.com>
2020-12-27 18:07:16 +0200

    update(components): style home page

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,28 +1,32 @@
 import React, { FC, useContext } from 'react'
-import { Button } from '@material-ui/core';
+import { Button, Typography } from '@material-ui/core';
 import { useHistory } from 'react-router-dom';
 import { AuthContext } from '../../components/AuthProvider';
 import { RouterPath } from '../../constants';
+import styles from './index.module.scss';
 
-type HomePageProps = {};
-
-export const HomePage: FC<HomePageProps> = () => {
+export const HomePage: FC = () => {
   const history = useHistory();
   const { onAuthStart, token } = useContext(AuthContext);
   const goToBrowserPage = () => history.push(RouterPath.Browser);
   const signIn = () => onAuthStart();
 
   return(
-    <article>
-      <h2>Home Page</h2>
+    <article className={styles.homePage}>
+      <Typography
+        variant="h3"
+        className={styles.heading}
+      >
+        Welcome to the fantastic Github Browser
+      </Typography>
       {token
         ? <Button
-        variant="contained"
-        color="primary"
-        onClick={goToBrowserPage}
-      >
-        Go to Browser
-      </Button>
+          variant="contained"
+          color="primary"
+          onClick={goToBrowserPage}
+        >
+          Go to Browser
+        </Button>
         : <Button
           variant="contained"
           color="primary"
@@ -30,7 +34,6 @@ export const HomePage: FC<HomePageProps> = () => {
         >
           Sign in
         </Button>}
-
     </article>
   );
 };
